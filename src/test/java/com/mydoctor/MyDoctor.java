7aaf46647509e0a0d287423b87a1a104efb2a10b
Gaurav Saste <gaurav.saste@zensar.com>
2018-02-08 12:49:05 +0530

    changes in code

--- src/test/java/com/mydoctor/MyDoctor.java
+++ src/test/java/com/mydoctor/MyDoctor.java
@@ -16,21 +16,28 @@ public class MyDoctor {
 	@BeforeTest
 	public void OpenBroweser(){
 		driver= new FirefoxDriver();
+		System.out.println("opening browser");
+		 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://35.225.248.24");
-		
+		System.out.println("opening url https://35.225.248.24 ");
 	}
 	
 @Test()
 public void verify_user_able_to_click_creatDoctor_Link(){
 	driver.findElement(By.xpath("//*[@id='leftcolumn']/div/li[3]/a")).click();
+	System.out.println("clicking on login button");
 	driver.findElement(By.xpath("//input[@name='id']")).sendKeys("10000");
 	driver.findElement(By.xpath("//input[@name='password']")).sendKeys("password");
 	driver.findElement(By.xpath("//input[@id='login']")).click();
+	System.out.println("enter user name and password and click on ok button");
 	driver.findElement(By.xpath("//*[@id='navlist']/li[2]/a")).click();
+	System.out.println("click on create doctor link");
 	String s1=driver.findElement(By.xpath("//div[@id='contentcolumn']/h2")).getText();
 	Assert.assertEquals(s1, "Create Doctor");
+	System.out.println("after clicking on create doctor link verify text Create Doctor");
+	/*System.out.println("verify create doctor link");*/
 	
 	
 	
@@ -39,9 +46,10 @@ public void verify_user_able_to_click_creatDoctor_Link(){
 @AfterTest
 public void logout(){
 	driver.findElement(By.xpath("//*[@id='navlist']/li[1]/a")).click();
+	System.out.println("click on Home button");
 	driver.findElement(By.xpath("//*[@id='navlist']/li[7]/a")).click();
+	System.out.println("Click on logout Button");
 	driver.findElement(By.xpath("//*[@id='login']")).click();
 	driver.quit();
 }
-
 }
