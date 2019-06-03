f539f1f6938d65c7cddc0fe91e10359ec1cdbc98
zencdoh <adminhybridit@imsrndlabs.com>
2019-06-03 21:53:14 +0530

    updates
    Bala updated

--- src/test/runTest.java
+++ src/test/runTest.java
@@ -10,6 +10,6 @@ import cucumber.junit.Cucumber;
 //Specifying pretty as a format option ensure that HTML report will be generated. 
 //When we specify html:target/Destination - It will generate the HTML report 
 
-inside the Destination folder, in the target folder of the maven project. 
+//inside the Destination folder, in the target folder of the maven project. 
 
 public class runTest { }
