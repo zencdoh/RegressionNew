acdb9b8147364f6115f551f5d291ff5e13b90094
zencdoh <adminhybridit@imsrndlabs.com>
2019-06-04 12:09:33 +0530

    updation
    Bala updated annotation

--- src/test/cucumberReport.java
+++ src/test/cucumberReport.java
@@ -3,10 +3,9 @@ package CucumberReport;
 import org.openqa.selenium.By; 
 import org.openqa.selenium.WebDriver; 
 import org.openqa.selenium.firefox.FirefoxDriver; 
-
-import cucumber.annotation.en.Given; 
-import cucumber.annotation.en.Then; 
-import cucumber.annotation.en.When; 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class cucumberReport { 
    WebDriver driver = null; 
